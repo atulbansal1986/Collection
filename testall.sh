35f3672881d8f3c8f16ee2fc37087a2eaece94fe
Atul Bansal <atulbansal1986@gmail.com>
2016-05-13 00:49:50 +0530

    testall script

---
+++ testall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -i
+rm a.out
+source ~/.bashrc
+for filename in $(find . -type f -name "*.x.cpp"); do
+d $filename
+./a.out > /dev/null
+if [ $? = 0 ]; then
+  echo "$filename passed"
+else
+  echo "$filename failed"
+fi
+done
